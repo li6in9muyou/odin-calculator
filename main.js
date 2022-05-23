0e619a54baf225c215681bbe84044e71d960f74c
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-05-23 15:09:06 +0800

    refactor: use case composition

--- main.js
+++ main.js
@@ -1,106 +1,4 @@
 // domain.js
-const Operator = {
-  subtract: {
-    text: "s",
-    operate: (left, right) => left - right,
-  },
-  add: {
-    text: "a",
-    operate: (left, right) => left + right,
-  },
-  divide: {
-    text: "d",
-    operate: (left, right) => left / right,
-  },
-  multiply: {
-    text: "m",
-    operate: (left, right) => left * right,
-  },
-  addCharacter(ch) {
-    function chToOperator(c) {
-      for (const op in Operator) {
-        if (Operator[op].text === c) {
-          return Operator[op];
-        }
-      }
-    }
-
-    switch (ch) {
-      case Operator.add.text:
-      case Operator.divide.text:
-      case Operator.multiply.text:
-      case Operator.subtract.text: {
-        return chToOperator(ch);
-      }
-      default:
-        throw `Invalid operator: ${ch}`;
-    }
-  },
-};
-
-class Decimal {
-  text = "";
-
-  addCharacter(ch) {
-    if (!ch.match(/[\d.-]/)) {
-      throw "Accepts only digits and dot sign, minus sign";
-    }
-    if (ch === "-" && this.text !== "") {
-      throw "Accepts minus sign only as first character";
-    }
-    if (ch === "." && this.text.indexOf(".") !== -1) {
-      throw "Accepts only one dot sign";
-    }
-    this.text += ch;
-  }
-
-  popCharacter() {
-    this.text = this.text.substring(0, this.text.length - 1);
-  }
-
-  checkValidText() {
-    return !isNaN(Number(this.text));
-  }
-
-  evaluate() {
-    if (this.checkValidText()) {
-      return Number(this.text);
-    }
-    throw `Bad text for a Decimal ${this.text}`;
-  }
-}
-
-class BinaryExpression {
-  operator;
-  leftExpression;
-  rightExpression;
-
-  get text() {
-    return (
-      this.leftExpression.text + this.operator.text + this.rightExpression.text
-    );
-  }
-
-  setOperator(ch) {
-    this.operator = ch;
-  }
-
-  setLeftExpression(expr) {
-    this.leftExpression = expr;
-  }
-
-  setRightExpression(expr) {
-    this.rightExpression = expr;
-  }
-
-  evaluate() {
-    return this.operator.operate(
-      this.leftExpression.evaluate(),
-      this.rightExpression.evaluate()
-    );
-  }
-}
-
 //service
 class EnterBinaryOperatorService {
   validOperator = {
@@ -136,7 +34,7 @@ class EnterBinaryOperatorService {
   }
 
   chToOperator(c) {
-    for (const [_, op] in Object.entries(this.validOperator)) {
+    for (const [_, op] of Object.entries(this.validOperator)) {
       if (op.text === c) {
         return op.operate;
       }
@@ -157,66 +55,91 @@ class EnterBinaryOperatorService {
 }
 
 class EnterDecimalService {
-  current;
-
-  constructor() {
-    this.current = new Decimal();
-  }
+  text = "";
 
   addCharacter(ch) {
-    try {
-      this.current.addCharacter(ch);
-    } catch (e) {
-      console.error(e);
-      ConsolePort(this.current);
+    if (!ch.match(/[\d.-]/)) {
+      throw "Accepts only digits and dot sign, minus sign";
     }
+    if (ch === "-" && this.text !== "") {
+      throw "Accepts minus sign only as first character";
+    }
+    if (ch === "." && this.text.indexOf(".") !== -1) {
+      throw "Accepts only one dot sign";
+    }
+    this.text += ch;
+  }
+
+  popCharacter() {
+    this.text = this.text.substring(0, this.text.length - 1);
+  }
+
+  checkValidText() {
+    return !isNaN(Number(this.text));
+  }
+
+  evaluate() {
+    if (this.checkValidText()) {
+      return Number(this.text);
+    }
+    throw `Bad text for a Decimal ${this.text}`;
   }
 }
 
 class EnterBinaryExpressionService {
-  current;
-  left_op;
+  tokens = [];
   op;
-  right_op;
-  left_done = false;
-  op_done = false;
-  right_done = false;
+  lhs;
+  rhs;
+  curr;
 
   constructor() {
-    this.current = new BinaryExpression();
-    this.left_op = new Decimal();
-    this.right_op = new Decimal();
+    this.op = new EnterBinaryOperatorService();
+    this.lhs = new EnterDecimalService();
+    this.rhs = new EnterDecimalService();
+    this.tokens = [this.lhs, this.op, this.rhs];
+    this.curr = 0;
   }
 
   addCharacter(ch) {
-    if (!this.left_done) {
-      try {
-        return !this.left_done && this.left_op.addCharacter(ch);
-      } catch (e) {
-        this.current.setLeftExpression(this.left_op);
-        this.left_done = true;
+    const currToken = this.tokens[this.curr];
+    try {
+      currToken.addCharacter(ch);
+    } catch (e) {
+      if (currToken.checkValidText()) {
+        ++this.curr;
+        this.addCharacter(ch);
+      } else {
+        console.error("enters", ch, "into", currToken);
+        throw "Bad";
       }
     }
+  }
 
-    if (!this.op_done) {
-      this.op = Operator.addCharacter(ch);
-      this.current.setOperator(this.op);
-      this.op_done = true;
-      return;
-    }
+  get text() {
+    return this.lhs.text + this.op.text + this.rhs.text;
+  }
 
-    if (!this.right_done) {
-      try {
-        return !this.right_done && this.right_op.addCharacter(ch);
-      } catch (e) {
-        this.current.setRightExpression(this.right_op);
-        this.right_done = true;
-      }
+  evaluate() {
+    return this.op.operate(this.lhs.evaluate(), this.rhs.evaluate());
+  }
+}
+
+class PerformCalculationService {
+  expression;
+
+  constructor() {
+    this.expression = new EnterBinaryExpressionService();
+  }
+
+  addCharacter(ch) {
+    if (ch !== "$") {
+      return this.expression.addCharacter(ch);
     }
   }
 
   evaluate() {
-    return this.current.evaluate();
+    return this.expression.evaluate();
   }
 }
 
@@ -228,20 +151,14 @@ function StringAdapter(service, text) {
 }
 
 function ConsolePort(expr) {
-  if (expr instanceof Decimal) {
-    console.log(expr.text);
-  } else if (expr instanceof BinaryExpression) {
-    console.log(expr.text);
-  } else {
-    console.error(`Unknown Expression: ${JSON.stringify(expr)}`);
-  }
+  console.log(expr.text);
 }
 
 //tests
 const testcases = ["1234a567$", "1234s567$", "1234m567$", "1234d567$"];
 for (const testcase of testcases) {
-  let service = new EnterBinaryExpressionService();
+  let service = new PerformCalculationService();
   StringAdapter(service, testcase);
-  ConsolePort(service.current);
+  ConsolePort(service.expression);
   console.log("result:", service.evaluate());
 }
